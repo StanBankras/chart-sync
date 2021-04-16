e0798707968b14b38807c4c18abe67aa98d23641
Stan Bankras <stanbankras@gmail.com>
2021-04-16 15:36:54 +0200

    Fix build steps

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const http = require('http').createServer(app);
 const path = require('path');
 const io = require('socket.io')(http, {
   cors: {
-    origin: "http://localhost:8080",
+    origin: `http://localhost:${process.env.PORT || '8080'}`,
     methods: ["GET", "POST"]
   }
 });
